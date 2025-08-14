62545bb66d305e23917357951487730239b9210a
Jennifer Kumar <67869018+jennykumar@users.noreply.github.com>
2025-08-14 09:44:46 +0530

    Create CodeColour.h

---
+++ CodeColour.h
@@ -0,0 +1,12 @@
+#ifndef COLORCODEUTILS_H
+#define COLORCODEUTILS_H
+
+namespace TelCoColorCoder
+{
+    extern const char* MajorColorNames[];
+    extern const char* MinorColorNames[];
+    extern const int numberOfMajorColors;
+    extern const int numberOfMinorColors;
+}
+
+#endif
