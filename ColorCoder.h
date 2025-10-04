3bb0d99020c7b1bdd73fea59ea27200da5864c85
Jennifer Kumar <67869018+jennykumar@users.noreply.github.com>
2025-10-04 21:42:09 +0530

    Create ColorCoder.h

---
+++ ColorCoder.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "ColorPair.h"
+
+namespace TelCoColorCoder
+{
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+}
