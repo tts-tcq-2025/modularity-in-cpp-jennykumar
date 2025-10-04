c7f0c020e21d76c3c6a8a1bb76c0cab4a8b8eb6a
Jennifer Kumar <67869018+jennykumar@users.noreply.github.com>
2025-10-04 21:38:03 +0530

    Delete ColorCoder.h

--- ColorCoder.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ColorPair.h"
-
-namespace TelCoColorCoder
-{
-    ColorPair GetColorFromPairNumber(int pairNumber);
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-}
